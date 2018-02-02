220004564b72a7d256c59d6f54be5d6e4c6f661f
Fabien Billard <fabien.billard01@gmail.com>
2018-02-02 10:46:13 +0100

    [M] Deleted hover on dashboard menu

--- imports/ui/components/LeftSidebar.js
+++ imports/ui/components/LeftSidebar.js
@@ -6,7 +6,7 @@ class LeftSidebar extends Component {
     render() {
         return (
             <div className="left">
-                <LeftSideItem icon="dashboard" label="DASHBOARD" class="item text-center"/>
+                <LeftSideItem icon="dashboard" label="DASHBOARD" class="item text-center item-no-hover"/>
                 <LeftSideItem icon="list" label="Tests list" class="item active"/>
                 {this.isLogged ? '' : <LeftSideItem icon="person_add" label="Register" class="item"/>}
                 {this.isLogged ? '<LeftSideItem icon="output" label="Logout" class="item" />' : <LeftSideItem icon="input" label="Login" class={"item"}/>}
