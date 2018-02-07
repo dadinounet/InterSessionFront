39b89b98152b76800deccb7559d1b6d5eac1d7fc
CyNn0 <boris.sb@laposte.net>
2018-02-07 10:44:51 +0100

    [+] add donate btn to left sidebar

--- imports/components/sidebar/LeftSidebar.js
+++ imports/components/sidebar/LeftSidebar.js
@@ -13,6 +13,7 @@ class LeftSidebar extends Component {
                 {this.isLogged ? '' : <Link to={`/register`}><LeftSideItem icon="person_add" label="Register" class="item"/></Link>}
                 {this.isLogged ? <Link to={`/logout`}><LeftSideItem icon="output" label="Logout" class="item"/></Link> : <Link to={`/login`}><LeftSideItem icon="input" label="Login" class={"item"}/></Link>}
                 <Link to={`/scanner`}><LeftSideItem icon="format_list_bulleted" label="List" class="item"/></Link>
+                <a target="_blank" href="https://paypal.me/itakedemy"><LeftSideItem icon="payment" label="Donate" class="item"/></a>
             </div>
         );
     }
