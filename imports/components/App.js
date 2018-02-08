a39b2a5e8bfc3fb02444b07e0f22d75dc1ac905b
Fabien Billard <fabien.billard01@gmail.com>
2018-02-08 21:22:32 +0100

    [M] Modified : routes

--- imports/components/App.js
+++ imports/components/App.js
@@ -1,46 +1,13 @@
 import React from 'react';
-import {BrowserRouter, Route, Redirect} from 'react-router-dom';
-import {connect} from 'react-redux';
-import Forms from "./forms/Forms";
+import {BrowserRouter, Route} from 'react-router-dom';
 import TestConfig from "./forms/TestConfig";
 import Sidebar from "./sidebar/Sidebar";
 import Navbar from "./sidebar/Navbar";
-
-const PrivateRoute = ({ component: Component, ...rest }) => (
-    <Route {...rest} render={(props) => (
-        Auth.user.isAuthenticated === true
-            ? <Component {...props} />
-            : <Redirect to={{
-                pathname: '/login',
-                state: { from: props.location }
-            }} />
-    )} />
-);
-
-const Auth = {
-    user: {
-        isAuthenticated: false,
-        token: null,
-        email: null,
-        history: []
-    },
-    authenticate() {
-        this.user.isAuthenticated = true
-        //todo check for a the real json
-    },
-    signout() {
-        this.user.isAuthenticated = false
-    }
-}
+import Login from "./forms/Login";
+import Register from "./forms/Register";
+import Logout from "./forms/Logout";
 
 class App extends React.Component {
-
-    state = {
-        user : {
-            isConnected: true
-        }
-    };
-
     render() {
         return (
             <BrowserRouter>
@@ -48,30 +15,19 @@ class App extends React.Component {
                     <Navbar/>
                     <Sidebar/>
                     <div className="row"/>
-                    {/* todo : secure routes */}
                     <div id="main-wrapper" className="row">
-                        <Route path="/login" render={() => <Forms userLogin={this.props.userLogin}/>}/>
-                        <Route path="/register" render={() => <Forms userRegister={this.props.userRegister}/>}/>
-                        <Route path="/password" component={Forms}/>
-                        <Route path="/scanner" render={() => <TestConfig submitGithubLink={this.props.submitGithubLink}/>}/>
+                        <div className="row align-self-center" id="securityScanner_forms">
+                            {/*todo : secure routes*/}
+                            <Route path="/" exact component={Login}/>
+                            <Route path="/login" component={Login}/>
+                            <Route path="/logout" component={Logout}/>
+                            <Route path="/register" component={Register}/>
+                            <Route path="/scanner" component={TestConfig}/>
+                        </div>
                     </div>
                 </main>
             </BrowserRouter>);
     }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        users: state.users
-    };
-};
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        submitGithubLink: (github) => {
-            dispatch({type: 'HTTP_REQUEST_GITHUB', data: github});
-        }
-    }
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
